8d8400803ce83f82307313fc3c7504f6fa7dad9c
Shreyas Sharma P <57483308+shreyas329@users.noreply.github.com>
2022-09-20 08:49:53 +0530

    Default values of primitive data type.

---
+++ PrimitiveDataType.java
@@ -0,0 +1,26 @@
+package com.bridgelabz;
+
+public class PrimitiveDataType {
+	static boolean a;
+	static char b;
+	static byte c;
+	static short d;
+	static int e;
+	static long f;
+	static float g;
+	static double h;
+
+	public static void main(String[] args) {
+		System.out.println("Default values of Primitive data types.....");
+		System.out.println("Value of boolean: "+ a);
+		System.out.println("Value of char: "+ b);
+		System.out.println("Value of byte: "+ c);
+		System.out.println("Value of short: "+ d);
+		System.out.println("Value of int: "+ e);
+		System.out.println("Value of long: "+ f);
+		System.out.println("Value of float: "+ g);
+		System.out.println("Value of double: "+ h);
+
+	}
+
+}
